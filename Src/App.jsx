15deb05fc9e1dc49469f6755424d8115ca651c0b
Ian Marini <135738365+IanMarini@users.noreply.github.com>
2025-04-27 20:58:11 -0300

    Add files via upload

---
+++ Src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import Actividad1 from './Actividades/Actividad1';
+import Actividad2 from './Actividades/actividad2';
+import Actividad3 from './Actividades/Actividad3';
+import Actividad4 from './Actividades/Actividad4';
+import Actividad5 from './Actividades/Actividad5';
+import Actividad6 from './Actividades/Actividad6';
+import Actividad7 from './Actividades/Actividad7';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Guía Diagnóstica</h1>
+      <Actividad1 />
+      <Actividad2 />
+      <Actividad3 />
+      <Actividad4 />
+      <Actividad5 />
+      <Actividad6 />
+      <Actividad7 />
+    </div>
+  );
+}
+
+export default App;
